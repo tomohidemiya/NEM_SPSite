696eba316ac84197ef14a2a10280a4de9c6e77ed
miyatomo <t.miya19890131@gmail.com>
2020-12-13 17:04:55 +0900

    typo

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an signup page</h1>
+  <div class="signup">
+    <div>
+      Signupは現在開発中です。<br>
+      アカウント追加をご希望の方は、ツイベガかみやともまでDMをいただけると嬉しいです。
+    </div>
   </div>
 </template>
