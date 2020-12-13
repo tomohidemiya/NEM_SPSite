c32b277918139b1e4a36aa1f69237350ce11bcb9
miyatomo <t.miya19890131@gmail.com>
2020-12-13 17:06:47 +0900

    typo

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="signup">
     <div>
-      Signupは現在開発中です。<br>
-      アカウント追加をご希望の方は、ツイベガかみやともまでDMをいただけると嬉しいです。
+      <p>Signupは現在開発中です。<br>
+      アカウント追加をご希望の方は、ツイベガかみやともまでDMをいただけると嬉しいです。</p>
+
+      <h3>Contact</h3>
+      <p><a href="https://twitter.com/tuibegakun" target="_blank" rel="noopener">ツイベガ君</a></p>
+      <p><a href="https://twitter.com/miyatomo_38" target="_blank" rel="noopener">みやとも</a></p>
     </div>
   </div>
 </template>
